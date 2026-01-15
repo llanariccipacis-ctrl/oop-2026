b5a70164d0eaa58e5e3c13f2dc818eda15177ef3
llanariccipacis-ctrl <llanaricci.pacis@lorma.edu>
2026-01-15 12:04:22 +0800

    Second Activity

---
+++ Mainn.java
@@ -0,0 +1,20 @@
+public class Mainn {
+    public static void main(String[] args) {
+        Mug firstmug = new Mug();
+        firstmug.component = "Clay";
+        firstmug.color = "Mint Green";
+        
+        Mug secondmug = new Mug();
+        secondmug.component = "Ceramic";
+        secondmug.color = "Maroon";
+
+        firstmug.printDetails();
+        secondmug.printDetails();
+
+        
+
+
+
+    }
+
+}
